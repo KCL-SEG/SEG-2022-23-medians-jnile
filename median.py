3519df147835aa0b5bcf10a9d540e2ebcbda7252
Jaidev Nileshkumar <jaidev.nileshkumar@kcl.ac.uk>
2022-09-24 18:16:20 +0100

    Add solution to find median

--- median.py
+++ median.py
@@ -1,5 +1,4 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 while True:
     try:
@@ -7,6 +6,18 @@ while True:
         numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
+        break
     else:
         break
-print(numbers)
+
+#Sort the numbers list
+numbers.sort()
+#find the middle number
+if(len(numbers)%2 == 0):
+    #2 middle numbers
+    leftNo = numbers[int(len(numbers)/2 - 1)]
+    rightNo = numbers[int(len(numbers)/2)]
+    print((leftNo + rightNo)/2)
+else:
+    #1 middle number
+    print(numbers[int((len(numbers)-1)/2)])
